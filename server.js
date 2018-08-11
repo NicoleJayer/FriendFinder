6b67a914447ea119be23efab0bab5416469284c1
Nicole Richardson <njrichardson95@gmail.com>
2018-08-11 14:16:51 -0400

    updates to server file

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ var bodyParser = require("body-parser");
 
 //code for using express application
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 8100;
 
 
 // code for using body parser in the application
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
+
+
+app.listen(PORT, function(){
+   console.log("Started our server and listening");
+})
