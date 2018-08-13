4fa7a7eef4ede3d797cd291133b54d92bff5acd6
Nicole Richardson <njrichardson95@gmail.com>
2018-08-13 12:50:09 -0400

    added in server files to connect with application files and use CSS

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8100;
 
+// Expose the public directory to access CSS files
+app.use(express.static(path.join(__dirname, './app/public')));
 
 // code for using body parser in the application
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
 app.listen(PORT, function(){
    console.log("Started our server and listening");
